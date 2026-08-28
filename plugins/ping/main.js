d29d0fb3721880a701676b9ee6c32c15060dd30f
404Navdeep <404navdeep@gmail.com>
2026-08-28 22:17:42 +0530

    meow

--- plugins/ping/main.js
+++ plugins/ping/main.js
@@ -1,10 +1,10 @@
 export default {
     id: "ping",
     name: "Ping",
-    version: "0.1.0",
+    version: "0.2.0",
 
     async init(ctx) {
-        ctx.command.register({
+        ctx.commands.register({
             name: "ping",
             description: "Check Pulse's response time",
 
@@ -14,42 +14,76 @@ export default {
                 respond,
                 ctx
             }) {
-                const start = Date.now();
+                const start = performance.now();
+
                 await ack();
-                const afterAck = Date.now();
+
+                const afterAck = performance.now();
 
                 try {
+                    const slackStart = performance.now();
+
                     const message =
                         await ctx.slack.botClient.chat.postMessage({
                             channel: command.channel_id,
                             text: ":ping_pong-wx: Pong!"
                         });
-                    const responseTime = Date.now() - start;
+
+                    const slackTime = Math.round(
+                        performance.now() - slackStart
+                    );
+
+                    const pulseTime = Math.round(
+                        slackStart - afterAck
+                    );
+
+                    const totalTime = Math.round(
+                        performance.now() - start
+                    );
 
                     await ctx.slack.botClient.chat.update({
                         channel: command.channel_id,
                         ts: message.ts,
-                        text: `:ping_pong-wx: Pong! \`${responseTime}ms\``
+
+                        text:
+                            `:ping_pong-wx: Pong!\n` +
+                            `:fastchip_nobg: Pulse: \`${pulseTime}ms\`\n` +
+                            `:slack-gold: Slack: \`${slackTime}ms\`\n` +
+                            `:time-zone-fyi:  Total: \`${totalTime}ms\``
                     });
-                    ctx.logger.info({
-                        responseTime,
-                        ackTime: afterAck - start
-                    },"Ping Complete")
-                    } catch (error) {
-                        ctx.logger.error({
+
+                    ctx.logger.info(
+                        {
+                            pulseTime,
+                            slackTime,
+                            totalTime,
+                            ackTime: Math.round(
+                                afterAck - start
+                            )
+                        },
+                        "Ping Complete"
+                    );
+                } catch (error) {
+                    ctx.logger.error(
+                        {
                             error: {
                                 message: error.message,
                                 stack: error.stack
                             }
-                        },"Ping failed");
+                        },
+                        "Ping failed"
+                    );
 
-                        await respond({
-                            response_type: "ephemeral",
-                            text: ":x: Couldnt do the pong"
-                        })
-                    }
+                    await respond({
+                        response_type: "ephemeral",
+                        text: ":x: Couldnt do the pong"
+                    });
                 }
-            });
-            ctx.logger.info("Ping plugin initialized");
+            }
+        });
+
+        ctx.logger.info(
+            "Ping plugin initialized"
+        );
     }
 };
